8f29a9f7bc24539fbef39797377a5d30ec9c3ce3
IDGayazov <118215176+IDGayazov@users.noreply.github.com>
2023-12-22 10:33:44 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import java.io.*;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main{
     public static boolean isVowel(char c){
         if(c == 'o' || c == 'a' || c == 'e' ||
